6d3ab3a5bb3c61b31c9864e6b9adc9c53c57085a
E. Afanasyeva <81641526+alldsaiw@users.noreply.github.com>
2022-04-20 17:16:38 +0300

    redesign
    Redesigned site, added hanburger menu script

---
+++ script/script.js
@@ -0,0 +1,13 @@
+let menuButton = document.querySelector('.h-menu');
+let menuShow = document.querySelector('.display-menu');
+
+menuButton.addEventListener('click', () => { 
+    if (menuShow.classList.contains('display-menu')) {
+    menuShow.classList.add('display-menu-open');
+    menuShow.classList.remove('display-menu');} 
+    else {
+        menuShow.classList.remove('display-menu-open');
+        menuShow.classList.add('display-menu');
+    }
+});
+
