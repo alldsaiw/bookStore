2095ec4faaab7d29b47037762631f8424e377fc8
E. Afanaseva <milianalynx@gmail.com>
2022-04-20 19:05:07 +0300

    minor changes

--- script/script.js
+++ script/script.js
@@ -1,13 +1,12 @@
 let menuButton = document.querySelector('.h-menu');
 let menuShow = document.querySelector('.display-menu');
 
-menuButton.addEventListener('click', () => { 
+menuButton.addEventListener('click', () => {
     if (menuShow.classList.contains('display-menu')) {
-    menuShow.classList.add('display-menu-open');
-    menuShow.classList.remove('display-menu');} 
-    else {
+        menuShow.classList.add('display-menu-open');
+        menuShow.classList.remove('display-menu');
+    } else {
         menuShow.classList.remove('display-menu-open');
         menuShow.classList.add('display-menu');
     }
 });
-
