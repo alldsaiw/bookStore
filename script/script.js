82d48b72f7b222a28ff94d4fc999ea9f51e2a43b
E. Afanaseva <milianalynx@gmail.com>
2022-05-11 20:30:43 +0300

    added cross icon

--- script/script.js
+++ script/script.js
@@ -1,5 +1,7 @@
 let menuButton = document.querySelector('.h-menu');
 let menuShow = document.querySelector('.display-menu');
+let hamMenu = document.querySelector('#menu-open');
+let menuCross = document.querySelector('#menu-close');
 
 menuButton.addEventListener('click', () => {
     if (menuShow.classList.contains('display-menu')) {
@@ -9,6 +11,13 @@ menuButton.addEventListener('click', () => {
         menuShow.classList.remove('display-menu-open');
         menuShow.classList.add('display-menu');
     }
+    if (menuShow.classList.contains('display-menu-open')) {
+        hamMenu.style.display = 'none';
+        menuCross.style.display = 'block';
+       } else {
+        hamMenu.style.display = 'block';
+        menuCross.style.display = 'none';
+       }
 });
 
 
